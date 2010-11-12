3411de0d9e7264918e7dce2d2b3a50ca2f9cac84
soda_2005 <soda_2005@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-12 17:11:35 +0000

    add unittest for user-function
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extracache/trunk@40130 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Tests/Typo3/UserFunc/EventTest.php
@@ -0,0 +1,75 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ *
+ * (c) 2010 AOE media GmbH <dev@aoemedia.de>
+ * All rights reserved
+ *
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+require_once dirname ( __FILE__ ) . '/../../AbstractDatabaseTestcase.php';
+
+/**
+ * test case for Tx_Extracache_Typo3_UserFunc_Event
+ * @package extracache_tests
+ * @subpackage Domain_Model
+ */
+class Tx_Extracache_Typo3_UserFunc_EventTest extends Tx_Extracache_Tests_AbstractDatabaseTestcase {
+	/**
+	 * @var array
+	 */
+	private $events;
+	/**
+	 * @var Tx_Extracache_Typo3_UserFunc_Event
+	 */
+	private $userFunc;
+	/**
+	 * @var Tx_Extracache_Domain_Repository_EventRepository
+	 */
+	private $mockedEventRepository;
+
+	/**
+	 * Prepares the environment before running a test.
+	 */
+	protected function setUp() {
+		$this->loadClass('Tx_Extracache_Typo3_UserFunc_Event');
+		$this->loadClass('Tx_Extracache_Domain_Repository_EventRepository');
+		
+		$this->events = array();
+		$this->events[] =  t3lib_div::makeInstance('Tx_Extracache_Domain_Model_Event', 'key1', 'aaaaa');
+		$this->events[] =  t3lib_div::makeInstance('Tx_Extracache_Domain_Model_Event', 'key2', 'ccccc');
+		$this->events[] =  t3lib_div::makeInstance('Tx_Extracache_Domain_Model_Event', 'key3', 'bbbbb');
+		
+		$this->mockedEventRepository = $this->getMock ( 'Tx_Extracache_Domain_Repository_EventRepository', array(), array(), '', FALSE);
+		$this->mockedEventRepository->expects($this->any())->method('getEvents')->will ( $this->returnValue ( $this->events ) );
+		$this->userFunc = $this->getMock ( 'Tx_Extracache_Typo3_UserFunc_Event', array('getEventRepository'));
+		$this->userFunc->expects($this->any())->method('getEventRepository')->will ( $this->returnValue ( $this->mockedEventRepository ) );
+		
+	}
+	/**
+	 * Cleans up the environment after running a test.
+	 */
+	protected function tearDown() {
+		unset ( $this->userFunc );
+		unset ( $this->mockedEventRepository );
+	}
+	/**
+	 * test method getEventItemsProcFunc
+	 * @test
+	 */
+	public function getEventItemsProcFunc() {
+		$parameters = array();
+		$parameters['items'] = array();
+		$this->userFunc->getEventItemsProcFunc($parameters);
+		
+		$this->assertTrue( count($parameters['items']) === 3);
+		$this->assertTrue( $parameters['items'][0][0] === 'aaaaa');
+		$this->assertTrue( $parameters['items'][0][1] === 'key1');
+		$this->assertTrue( $parameters['items'][1][0] === 'bbbbb');
+		$this->assertTrue( $parameters['items'][1][1] === 'key3');
+		$this->assertTrue( $parameters['items'][2][0] === 'ccccc');
+		$this->assertTrue( $parameters['items'][2][1] === 'key2');
+	}
+}
