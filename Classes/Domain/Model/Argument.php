513093a8d53714e0a6d3f0901a8a50b65448df39
soda_2005 <soda_2005@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-18 15:18:05 +0000

    add PHPdoc-comments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extracache/trunk@40414 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Model/Argument.php
+++ Classes/Domain/Model/Argument.php
@@ -14,9 +14,25 @@
  * @package extracache
  */
 class Tx_Extracache_Domain_Model_Argument {
+	/**
+	 * define arguments, which should be ignored if staticCache should be written or readed in:
+	 *  - Classes/Typo3/Hooks/StaticFileCache/CreateFileHook.php
+	 *  - Classes/System/StaticCache/AbstractManager.php
+	 */
 	const TYPE_ignoreOnCreatingCache = 'ignoreOnCreatingCache';
+	/**
+	 * define arguments, which makes sure that requests can't be processed/responsed by staticCache in: 
+	 *  - Classes/System/StaticCache/EventHandler.php
+	 */
 	const TYPE_unprocessible = 'unprocessible';
+	/**
+	 * define arguments, which should be saved if staticCache will be written and restored if request will be processed/responsed by staticCache in:
+	 *  - Classes/Typo3/Hooks/StaticFileCache/CreateFileHook.php
+	 */
 	const TYPE_whitelist = 'whitelist';
+	/**
+	 * 
+	 */
 	const TYPE_frontendConfig = 'frontendConfig';
 
 	/**
