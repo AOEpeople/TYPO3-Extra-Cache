75b0f14edd9ab47aab8f524d0236bdaac2d6f974
soda_2005 <soda_2005@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-19 08:56:50 +0000

    delete configuration which is needed for eft- and congstarizer-extension
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extracache/trunk@40437 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Bootstrap.php
+++ Classes/Bootstrap.php
@@ -29,11 +29,6 @@ final class Bootstrap {
 		self::initializeXClasses();
 */
 		self::initializeSchedulerTasks();
-
-		// this configurations must later be copied into the eft-extension
-		self::initializeDefaultArguments();
-		self::initializeDefaultCleanerStrategies();
-		self::initializeDefaultCacheCleanerEvents();
 	}
 	
 	/**
@@ -111,93 +106,6 @@ final class Bootstrap {
 	static protected function initializeXClasses() {
 		// Define XCLASS for nc_staticfilecache info module:
 		$GLOBALS['TYPO3_CONF_VARS']['BE']['XCLASS']['ext/nc_staticfilecache/infomodule/class.tx_ncstaticfilecache_infomodule.php'] = PATH_tx_extracache . 'Classes/Controller/ExtendedStaticFileCacheInfoModule.php';
-	}
-
-	/**
-	 * initialize some default cleanerStrategies
-	 */
-	static protected function initializeDefaultArguments() {
-		/** @var $configurationManager Tx_Extracache_Configuration_ConfigurationManager */
-		$configurationManager = t3lib_div::makeInstance('Tx_Extracache_Configuration_ConfigurationManager');
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_whitelist, 'eft', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_whitelist, 'eftdslconfig', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_whitelist, 'eftprepaidconfig', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_whitelist, 'tx_t3blog_pi1', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_whitelist, 'tx_aoesurvey_pi1', true);
-
-		$value = array('action' => array('getAdressAjax', 'getPaymentDebitDataAjax'));
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, '*', $value);
-		$value = array('action' => array('getCompanionProductsAjax', 'showMobilePhoneProductListAjax'));
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, 'eftproductchange', $value);
-		$value = array('action' => array('getCompanionProductsAjax','showMobilePhoneProductListAjax','updateCompanionProductOptionsAjax'));
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, 'eftPostpaidMobilePhone', $value);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, 'eID', true);
-		$value = array('action' => array('index', 'upload', 'publish', 'download', 'new', 'create'));
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, 'tx_congstarizer_pi1', $value);
-		$value = array('action' => array('create', 'update', 'delete'));
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_unprocessible, 'tx_congstarizer_pi2', $value);
-		
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, 'eftbasket', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, 'cHash', true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, 'ftu', true);
-
-		require_once(PATH_tx_eft.'domain/service/campaign/class.tx_eft_domain_service_campaign_affiliateEntryDetection.php');
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::AFFILINET_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::FRIEND_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::FRIEND_PARAMETER_STARSELLER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::GLOBAL_GROUP_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::GOOGLE_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::MISSIONONE_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::TKWORLD_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::YAHOO_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::ZANOX_PARAMETER, true);
-		$configurationManager->addArgument(Tx_Extracache_Domain_Model_Argument::TYPE_ignoreOnCreatingCache, tx_eft_domain_service_campaign_affiliateEntryDetection::ZED_PARAMETER, true);
-	}
-	/**
-	 * initialize some default cleanerStrategies
-	 */
-	static protected function initializeDefaultCleanerStrategies() {
-		/** @var $configurationManager Tx_Extracache_Configuration_ConfigurationManager */
-		$configurationManager = t3lib_div::makeInstance('Tx_Extracache_Configuration_ConfigurationManager');
-
-		$actions = Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_TYPO3Clear + Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_StaticUpdate;
-		$childrenMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ChildrenNoAction;
-		$elementsMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ElementsNoAction;
-		$key = '1';
-		$name = 'Update: Seite (ohne varianten)';
-		$configurationManager->addCleanerStrategy($actions, $childrenMode, $elementsMode, $key, $name);
-		
-		$actions = Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_TYPO3Clear + Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_StaticClear;
-		$childrenMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ChildrenNoAction;
-		$elementsMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ElementsOnly;
-		$key = '2';
-		$name = 'Clear: nur Seitenvarianten (Elemente)';
-		$configurationManager->addCleanerStrategy($actions, $childrenMode, $elementsMode, $key, $name);
-
-		$actions = Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_TYPO3Clear + Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_StaticClear;
-		$childrenMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ChildrenWithParent;
-		$elementsMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ElementsWithParent;
-		$key = '3';
-		$name = 'Clear: Seite und Unterseiten (alles)';
-		$configurationManager->addCleanerStrategy($actions, $childrenMode, $elementsMode, $key, $name);
-
-		$actions = Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_TYPO3Clear + Tx_Extracache_Domain_Model_CleanerStrategy::ACTION_StaticUpdate;
-		$childrenMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ChildrenNoAction;
-		$elementsMode = Tx_Extracache_Domain_Model_CleanerStrategy::CONSIDER_ElementsWithParent;
-		$key = '4';
-		$name = 'Update: Seite und Varianten der Seite';
-		$configurationManager->addCleanerStrategy($actions, $childrenMode, $elementsMode, $key, $name);
-	}
-	/**
-	 * initialize some default cacheCleanerEvents
-	 */
-	static protected function initializeDefaultCacheCleanerEvents() {
-		/** @var $configurationManager Tx_Extracache_Configuration_ConfigurationManager */
-		$configurationManager = t3lib_div::makeInstance('Tx_Extracache_Configuration_ConfigurationManager');
-		$configurationManager->addEvent('onUpdateProductCatalogueForClientId1', 'Produktkatalog-Update [client: congstar]');
-		$configurationManager->addEvent('onUpdateProductCatalogueForClientId2', 'Produktkatalog-Update [client: ebay]');
-		$configurationManager->addEvent('onUpdateProductCatalogueForClientId3', 'Produktkatalog-Update [client: RTL]');
-		$configurationManager->addEvent('onAddCongstarizerPicture', 'congstarizer-Bild in Gallerie einfügen');		
 	}
 	/**
 	 * initialize event-handler
