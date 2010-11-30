8b107b952a211b784b682f1c8184b3ca7b630c80
soda_2005 <soda_2005@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-30 17:00:54 +0000

    support event eventOnStaticCachePreprocess
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/extracache/branches/active-eventhandling@40688 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/System/Event/Events/EventOnStaticCachePreprocess.php
@@ -0,0 +1,29 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 AOE media GmbH <dev@aoemedia.de>
+*  All rights reserved
+*
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * this event will be thrown, before the staticCache-Dispatcher try to load the requested page from staticCache
+ * 
+ * @package extracache
+ * @subpackage System_Event_Events
+ */
+class Tx_Extracache_System_Event_Events_EventOnStaticCachePreprocess extends Tx_Extracache_System_Event_Events_Event {
+	/**
+	 * name of the event
+	 * @var string
+	 */
+	protected $name = 'onStaticCachePreprocess';
+
+	/**
+	 * override constructor
+	 */
+	public function __construct(){}
+}
