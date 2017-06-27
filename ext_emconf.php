9436e5a82ea1e09d61e177d9ed34fc13418c6661
Tomas Norre Mikkelsen <tomas.mikkelsen@aoe.com>
2017-06-27 15:04:22 +0200

    [TASK] CD-58109 Bump version 0.9.1 + updated contraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,10 +26,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'version' => '0.9.0',
+	'version' => '0.9.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-6.2.99',
+			'typo3' => '6.2.0-7.6.99',
 			'extbase' => '',
 			'nc_staticfilecache' => ''
 		),
